2f9c860b693c1ddad9e6218a731c7a2b1e3c187a
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-01 14:49:49 +0600

    added demo rake

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,7 +6,8 @@ scheduler = Rufus::Scheduler.new
 #     puts "This scheduler is printed every 10 seconds"
 # end
 
-scheduler.cron "54 7 * * *" do
+# runs everyday at 12 PM
+scheduler.cron "0 6 * * *" do
     User.where("last_login IS NULL OR last_login < ?", (1.day.ago + 6.hours)).destroy_all
     puts "Scheduler: Deleted inactive users who were active 1 day ago"
 end
